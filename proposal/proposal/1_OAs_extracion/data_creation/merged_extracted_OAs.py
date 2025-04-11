2ede6064547e00308f273e9cbab1bbe6e4b9bf00
vinhnk2920 <khanhvinh.work@gmail.com>
2025-04-11 11:13:29 +0900

    OA extraction

--- proposal/proposal/1_OAs_extracion/data_creation/merged_extracted_OAs.py
+++ proposal/proposal/1_OAs_extracion/data_creation/merged_extracted_OAs.py
@@ -4,9 +4,9 @@ import os
 merged_data = []
 
 # Adjust this path if files are in a subfolder
-folder_path = 'results/2nd_prompt/list_OAs/'  
+folder_path = 'results/1M/list_OAs/'  
 
-for i in range(1, 61):
+for i in range(1, 11):
     filename = os.path.join(folder_path, f"extracted_OAs_{i}.json")
     with open(filename, 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -18,5 +18,5 @@ for i in range(1, 61):
             print(f"Unknown format in file: {filename}")
 
 # Save merged data
-with open('results/2nd_prompt/extracted_OAs.json', 'w', encoding='utf-8') as f:
+with open('results/1M/extracted_OAs_500k.json', 'w', encoding='utf-8') as f:
     json.dump(merged_data, f, ensure_ascii=False, indent=2)
