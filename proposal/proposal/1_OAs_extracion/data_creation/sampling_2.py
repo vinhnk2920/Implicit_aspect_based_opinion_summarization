11703dbb22728a11a2a77a83f3d9ef44777cb850
vinhnk2920 <khanhvinh.work@gmail.com>
2025-05-03 16:35:28 +0900

    proposal sampling

--- proposal/proposal/1_OAs_extracion/data_creation/sampling_2.py
+++ proposal/proposal/1_OAs_extracion/data_creation/sampling_2.py
@@ -123,8 +123,8 @@ def create_mix_structured_data(oas_data, iss_data, embeddings, embedding_matrix)
 # === Main Script === #
 if __name__ == "__main__":
     glove_file = "glove/glove.6B.300d.word2vec.txt"
-    oas_file = "results/1M/extracted_OAs/extracted_OAs_1M.json"
-    output_file = "results/1M/mix_structured_data/list/mix_structured_data_1M_2.json"
+    oas_file = "results/1M_random/extracted_OAs_1M_random.json"
+    output_file = "results/1M_random/mix_structured_data/mix_structured_data_1M_random_2.json"
     
     embeddings, embedding_matrix = load_glove_embeddings(glove_file)
     
